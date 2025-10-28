0436ca0065e257113a6817fa397aebcea2fd9494
Huizhi Xu <xuhuizhi@hotmail.com>
2025-10-28 17:58:09 +0800

    rebuilding site Tue Oct 28 17:58:03 CST 2025

---
+++ deploy copy.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -eux
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+msg="rebuilding site `date`"
+if [ $# -eq 1  ]
+    then msg="$1"
+fi
+
+# Build the project.
+hugo --buildDrafts=false -t blist # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add . -f
+
+# Commit changes.
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
+
+git add .
+git commit -m "$msg"
+git push origin master
